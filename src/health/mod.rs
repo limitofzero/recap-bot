74b065ffa53a1be91f9becaa97840ecbe3f21b07
Denis Makarov <limitofzero@gmail.com>
2026-05-10 22:42:21 +0400

    lint: fmt

--- src/health/mod.rs
+++ src/health/mod.rs
@@ -1,10 +1,8 @@
-use axum::{Router, extract::State, http::StatusCode, routing::get};
+use axum::{extract::State, http::StatusCode, routing::get, Router};
 use sqlx::PgPool;
 
 pub async fn run(pool: PgPool, port: u16) -> std::io::Result<()> {
-    let app = Router::new()
-        .route("/health", get(health))
-        .with_state(pool);
+    let app = Router::new().route("/health", get(health)).with_state(pool);
 
     let listener = tokio::net::TcpListener::bind(("0.0.0.0", port)).await?;
     log::info!("health server is stared on port: {}", port);
@@ -12,8 +10,7 @@ pub async fn run(pool: PgPool, port: u16) -> std::io::Result<()> {
 }
 
 async fn health(State(pool): State<PgPool>) -> StatusCode {
-    let request = sqlx::query("SELECT 1")
-        .execute(&pool);
+    let request = sqlx::query("SELECT 1").execute(&pool);
     match request.await {
         Ok(_) => StatusCode::OK,
         Err(err) => {
